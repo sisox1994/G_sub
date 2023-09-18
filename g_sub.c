964cb460989fb23b0ed09100ef2392ba8f1a4068
EACT-Bill <114469405+EACT-Bill@users.noreply.github.com>
2023-09-18 09:28:23 +0800

    [更新] Ver1.1

--- g_sub.c
+++ g_sub.c
@@ -1,6 +1,13 @@
 #include "g_sub.h"
 
+#define LIB_VERSION "1.1"
+
 void gg_inin_der(){
 
 	printf("GG IN in Der\n");
+}
+
+void gg_super_inin_der(){
+
+	printf("GG Super IN in Der\n");
 }
--- g_sub.h
+++ g_sub.h
@@ -1,7 +1,7 @@
 #ifndef G_SUB_H__
 #define G_SUB_H__
 
-void gg_inin_der();
-
+void gg_inin_der(void);
+void gg_super_inin_der(void);
 
 #endif // G_SUB_H__
